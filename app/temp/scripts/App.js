4ef26506c36decd30588342de3347406f8510fcc
Łukasz <lukasz.stefanski@e-kwiaty.pl>
2018-08-14 09:56:13 +0200

    Reveal On Scroll really completed

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -10345,10 +10345,15 @@ var _RevealOnScroll = __webpack_require__(4);
 
 var _RevealOnScroll2 = _interopRequireDefault(_RevealOnScroll);
 
+var _jquery = __webpack_require__(0);
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var mobileMenu = new _MobileMenu2.default();
-var revealOnScroll = new _RevealOnScroll2.default();
+new _RevealOnScroll2.default((0, _jquery2.default)(".feature-item"), "85%");
+new _RevealOnScroll2.default((0, _jquery2.default)(".testimonial"), "85%");
 
 /***/ }),
 /* 3 */
@@ -10426,10 +10431,11 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var RevealOnScroll = function () {
-	function RevealOnScroll() {
+	function RevealOnScroll(els, offset) {
 		_classCallCheck(this, RevealOnScroll);
 
-		this.itemsToReveal = (0, _jquery2.default)(".feature-item");
+		this.itemsToReveal = els;
+		this.offsetPercentage = offset;
 		this.hideInitially();
 		this.createWaypoints();
 	}
@@ -10442,6 +10448,7 @@ var RevealOnScroll = function () {
 	}, {
 		key: 'createWaypoints',
 		value: function createWaypoints() {
+			var that = this;
 			this.itemsToReveal.each(function () {
 				var currentItem = this;
 				new Waypoint({
@@ -10449,7 +10456,7 @@ var RevealOnScroll = function () {
 					handler: function handler() {
 						(0, _jquery2.default)(currentItem).addClass("reveal-item--is-visible");
 					},
-					offset: "85%"
+					offset: that.offsetPercentage
 				});
 			});
 		}
