a67fe62a2e0807e87b2372d7bed5cec37b8663c2
Łukasz <lukasz.stefanski@e-kwiaty.pl>
2018-08-14 15:09:19 +0200

    Added browser support fallbacks.

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
 webpack = require('webpack'),
 browserSync = require('browser-sync').create();
 
-gulp.task('scripts', function(callback){
+gulp.task('scripts', ['modernizr'], function(callback){
 	webpack(require('../../webpack.config.js'), function(err, stats) {
 		if (err){
 			console.log(err.toString());
