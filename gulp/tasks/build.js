77ed1370d4a4e7ea023d41b8960dfb9ca80afe93
lukasz-ek <lukasz.stefanski@e-kwiaty.pl>
2018-08-16 09:23:17 +0200

    Completed gulp build task.

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -4,10 +4,20 @@ del = require('del'),
 usemin = require('gulp-usemin'),
 rev = require('gulp-rev'),
 cssnano = require('gulp-cssnano'),
-uglify = require('gulp-uglify');
+uglify = require('gulp-uglify'),
+browserSync = require('browser-sync').create();
 
-gulp.task('deleteDistFolder', function() {
-	return del("./dist");
+gulp.task('previewDist', function() {
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "docs"
+		}
+	});	
+});
+
+gulp.task('deleteDistFolder', ['icons'], function() {
+	return del("./docs");
 });
 
 gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
@@ -21,29 +31,33 @@ gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
 		'!./app/temp/**'
 	];
 	return gulp.src(pathsToCopy)
-		.pipe(gulp.dest("./dist"));
+		.pipe(gulp.dest("./docs"));
 });
 
-gulp.task('optimizeImages', ['deleteDistFolder', 'icons'], function(){
+gulp.task('optimizeImages', ['deleteDistFolder'], function(){
 	return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
 		.pipe(imagemin({
 			progressive: true,
 			interlaced: true,
 			multipass: true
 		}))
-		.pipe(gulp.dest("./dist/assets/images"));
-
+		.pipe(gulp.dest("./docs/assets/images"));
 });
 
-gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function(){
+gulp.task('useminTrigger', ['deleteDistFolder'], function(){
+	gulp.start("usemin");
+});
+
+
+gulp.task('usemin', ['styles', 'scripts'], function(){
 	return gulp.src(['./app/index.html'])
 		.pipe(usemin({
 			css: [function() {return rev()}, function() {return cssnano()}],
 			js: [function() {return rev()}, function() {return uglify()}]
 		}))
-		.pipe(gulp.dest("./dist"));
+		.pipe(gulp.dest("./docs"));
 
 });
 
 
-gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'usemin']);
+gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'useminTrigger']);
